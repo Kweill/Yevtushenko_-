c8e8e288d50fd7dfb5ef8882356d8d7254aec28a
Kweill <144922671+Kweill@users.noreply.github.com>
2026-03-28 14:49:11 +0200

    Add files via upload

---
+++ Project_Yevtushenko/Core/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Core
+{
+    public class Transaction
+    {
+        public string Description { get; set; } = "";
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsIncome { get; set; }
+
+        public override string ToString()
+        {
+            return $"Description: {Description}, Amount: {Amount}, Date: {Date}, Income: {IsIncome}";
+        }
+    }
+}
